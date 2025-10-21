ce9cdeddcd56b8105bfe6cbacabce24e86372658
JxAmani <jeremyamani336@gmail.com>
2025-10-21 15:13:32 +0300

    Restructure page routing and add MedicLandingPage

--- medichain-frontend/src/App.jsx
+++ medichain-frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import MedicLandingPage from "./pages/MedicLandingPage";
 import PatientPage from "./pages/PatientPage";
 import OfficerPage from "./pages/OfficerPage";
 import CaregiverPage from "./pages/CaregiverPage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/medic" element={<MedicLandingPage />} />
         <Route path="/patient" element={<PatientPage />} />
         <Route path="/officer" element={<OfficerPage />} />
         <Route path="/caregiver" element={<CaregiverPage />} />
@@ -16,5 +18,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
